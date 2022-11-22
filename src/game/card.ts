4a0cf2c4cf6dfb0757fc74f42125a6a716caccad
qormsxo <45680973+qormsxo@users.noreply.github.com>
2022-11-22 19:04:33 +0900

    fix: 제너레이터 메소드 표현식 변경

--- src/game/card.ts
+++ src/game/card.ts
@@ -14,31 +14,33 @@ export class Card {
         }
     }
 
+
     private createCardDeck() {
         // 제너레이터
-        let createNum = function* () {
-            let i: number = 1;
-            let count: number = 0;
-            //조커를 제외한 52장
-            while (count < 52) {
-                yield i;
-                count++;
-                if (count >= 36) {
-                    // J Q K A 는 10인걸 고려해서 count 갯수로 숫자 판단
-                    i = 10;
-                } else if (count % 4 === 0) {
-                    // 문양이 4개이므로 문양마다 숫자를 넣었다면 숫자 증가
-                    i++;
-                }
-            }
-            return 0;
-        };
         // 3벌로 카드 덱만들기
         for (let i = 0; i < 3; i++) {
-            let iterator = createNum();
+            let iterator = this.createNum();
             for (let value of iterator) {
                 this.card.push(value);
             }
         }
     }
+    private * createNum() {
+        let i: number = 1;
+        let count: number = 0;
+        //조커를 제외한 52장
+        while (count < 52) {
+            yield i;
+            count++;
+            if (count >= 36) {
+                // J Q K A 는 10인걸 고려해서 count 갯수로 숫자 판단
+                i = 10;
+            } else if (count % 4 === 0) {
+                // 문양이 4개이므로 문양마다 숫자를 넣었다면 숫자 증가
+                i++;
+            }
+        }
+        return 0;
+    };
+
 }
